8c3cd6bc17ee9da613b03b659116e1cb0c76d51e
alinen <anormoyle@brynmawr.edu>
2024-08-28 17:00:34 -0400

    Getting Started Assignment

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("Hello World!");
+    }
+}
